42e0c9703079688ba5a993e4c69e9536c4917afb
Arthur Litnarovych <doubledoge246@gmail.com>
2023-08-26 20:51:31 +0300

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
