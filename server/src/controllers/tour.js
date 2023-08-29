e857a773f686af08ac5706f697634e82dc0676f1
Arthur Litnarovych <doubledoge246@gmail.com>
2023-08-29 22:25:14 +0300

    added tour system

--- server/src/controllers/tour.js
+++ server/src/controllers/tour.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import tourModel from "../models/tour.js";
 
 export const createTour = async (req, res) => {
@@ -23,4 +24,25 @@ export const getTours = async (req, res) => {
   } catch (error) {
     return res.status(404).json({ message: "Something went wrong" });
   }
+};
+
+export const getTour = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const tour = await tourModel.findById(id);
+    return res.status(200).json(tour);
+  } catch (error) {
+    return res.status(404).json({ message: "Something went wrong" });
+  }
+};
+
+export const getToursByUser = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "User doesn`t exist" });
+  }
+
+  const tours = await tourModel.find({ creator: id });
+  return res.status(200).json(tours);
 };
